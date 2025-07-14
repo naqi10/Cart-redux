803fbeaec532bab4a4257fc6d71a20d1c4624b35
naqi10 <naqizaidi2112002@gmail.com>
2025-07-14 15:46:45 +0500

    upload full react app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import products from "./products.json";
+import "./App.css";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+
+function App() {
+  return (
+    <div className="App">
+      <Cart />
+      <div className="products">
+        {products.map((product) => (
+          <Product {...product} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
